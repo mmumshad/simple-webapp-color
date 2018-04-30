7e6b3361a645743ff7f1d2d00c1ca8cd02b4d54a
Mumshad Mannambeth <mmumshad@gmail.com>
2018-05-01 07:12:38 +0800

    Initial Commit

---
+++ app.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask
+from flask import render_template
+import socket
+import random
+import os
+
+app = Flask(__name__)
+
+color_codes = {
+    "red": "#e74c3c",
+    "green": "#16a085",
+    "blue": "#2980b9",
+    "blue2": "#30336b",
+    "pink": "#be2edd",
+    "darkblue": "#130f40"
+}
+
+color = os.environ.get('APP_COLOR') or random.choice(["red","green","blue","blue2","darkblue","pink"])
+
+@app.route("/")
+def main():
+    #return 'Hello'
+    print(color)
+    return render_template('hello.html', name=socket.gethostname(), color=color_codes[color])
+
+@app.route('/color/<new_color>')
+def new_color(new_color):
+    return render_template('hello.html', name=socket.gethostname(), color=color_codes[new_color])
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="8080")
