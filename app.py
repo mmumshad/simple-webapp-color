eb4e4b8a01f06180fe824184e91b51cb62a5ea5f
Mumshad Mannambeth <mmumshad@gmail.com>
2018-05-01 20:12:31 +0800

    Update to read file

--- app.py
+++ app.py
@@ -28,5 +28,11 @@ def main():
 def new_color(new_color):
     return render_template('hello.html', name=socket.gethostname(), color=color_codes[new_color])
 
+@app.route('/read_file')
+def read_file():
+    f = open(".gitignore")
+    contents = f.read()
+    return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8080")
